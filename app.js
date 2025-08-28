72e72a654599cdfcf7dd95194e107328e0f1e00e
Nina Plejdrup <ninaplejdrup@hotmail.com>
2025-08-28 20:34:18 +0200

    Event bubbling/capturing

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const grandparent = document.querySelector(".grandparent");
 const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
 
-grandparent.style.width = "330px";
+grandparent.addEventListener("click", (e) => {
+  console.log("Grandparent 1"), { capture: true };
+});
+parent.addEventListener("click", (e) => {
+  console.log("Parent 1"), { capture: true };
+});
+child.addEventListener("click", (e) => {
+  console.log("Child 1"), { capture: true };
+});
+
+// [9:12]
