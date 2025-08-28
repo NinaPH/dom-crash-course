7ae16bd226af1a97bed45465e42efd5143dbbc9e
Nina Plejdrup <ninaplejdrup@hotmail.com>
2025-08-28 20:02:23 +0200

    renamed .js file and made changes to it

---
+++ app.js
@@ -0,0 +1,5 @@
+const grandparent = document.querySelector(".grandparent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+
+grandparent.style.width = "330px";
--- dom.js
+++
@@ -1,1 +0,0 @@
-const grandparent = document.querySelector();
