75bf2977d52b0bea79c2733feba3e7687e4c1eac
Nina Plejdrup <ninaplejdrup@hotmail.com>
2025-08-28 19:55:04 +0200

    started event handling vid

--- dom.js
+++ dom.js
@@ -1,10 +1,1 @@
-const playground = document.querySelector("#playground");
-
-const secretScroll = document.createElement("div");
-
-secretScroll.id = "secretScroll";
-secretScroll.dataset.spell = "invisibility";
-secretScroll.dataset.components = "moonlight, shadow essence";
-secretScroll.textContent = "Ancient Spell Scroll";
-
-playground.append(secretScroll);
+const grandparent = document.querySelector();
